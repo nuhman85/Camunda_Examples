2cfc17e5b26524730edc60dd40a26b3452db8a1b
Andre Mare <andre.mare@gmail.com>
2019-08-26 07:02:39 +0200

    Added BaseCamundaBPM Example

---
+++ SimpleCamundaBPM/BaseCamundaBPM/src/main/java/com/code2bits/camunda/simplebpm/SimpleCamundaBpmApplication.java
@@ -0,0 +1,15 @@
+package com.code2bits.camunda.simplebpm;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class SimpleCamundaBpmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleCamundaBpmApplication.class, args);
+	}
+
+}
